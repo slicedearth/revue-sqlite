80642fb37692bbcc3323a10f8c937e8c55eccfce
slicedearth <amarei@e46947-imac.local>
2019-10-10 11:41:01 +1100

    Updated App and Config
    Configured app and config files in server.

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const { sequelize } = require("./models");
+const config = require("./config/config");
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
+
+require("./routes.js")(app);
+
+sequelize.sync().then(() => {
+  app.listen(process.env.PORT || config.port);
+  console.log(`Server started on port ${config.port}`);
+});
