dc8107baa5794a0be838c2a08ab65d9c71bb7699
slicedearth <47583576+slicedearth@users.noreply.github.com>
2019-10-21 22:43:54 +1100

    Added Client Component Files
    Added client component files and updated user model to accept a username.

--- server/src/models/UserModel.js
+++ server/src/models/UserModel.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
     {
+      username:{
+        type: DataTypes.STRING,
+        unique: true
+      },
       email: {
         type: DataTypes.STRING,
         unique: true
